f4e038183e7dfbfb20e3f4f86b41bb1a409ac3da
daiqin <daiqin1046@gmail.com>
2025-08-19 22:32:51 +0800

    feat: 🔨 project init

--- rspress.config.ts
+++ rspress.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'rspress/config'
 
 export default defineConfig({
   root: path.join(__dirname, 'docs'),
-  title: 'My Site',
+  title: 'Caterpi11ar\'s Blog',
   icon: '/rspress-icon.png',
   logo: {
     light: '/rspress-light-logo.png',
